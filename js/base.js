e436adb4c82ffd6608b8192e2f10ff1f372abaff
Visionchen <384278250@qq.com>
2016-10-05 22:59:18 +0800

    5

--- js/base.js
+++ js/base.js
@@ -1,3 +1,25 @@
 /**
  * Created by Administrator on 2016/10/4.
  */
+window.onload = function() {
+    function $(id) {
+        return document.getElementById(id);
+    }
+
+    var pop_top = $('pop_top').getElementsByTagName('a');
+var pop_com = $('pop_com').getElementsByTagName('li');
+for (var i = 0; i < pop_top.length; i++) {
+    pop_top[i].indexs= i;
+    pop_top[i].onmouseover = function () {
+        for (var j = 0; j < pop_top.length; j++) {
+            if (j == this.indexs) {
+                this.className = 'focus';
+                pop_com[j].style.display = 'block';
+            } else {
+                pop_top[j].className = '';
+                pop_com[j].style.display='none';
+            }
+        }
+    }
+}
+};
--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 /**
  * Created by Administrator on 2016/10/4.
  */
-var taxl =$('tax-left');
-var taxm =$('tax-middle');
-var taxr =$('tax-right');
+
+    var taxl = $('tax-left');
+    var taxm = $('tax-middle');
+    var taxr = $('tax-right');
+/////////////////////////////
